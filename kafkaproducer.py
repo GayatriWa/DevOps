f6dbf9bd5ba88e3485fe6ab157bba50422bf6ca3
GayatriWa <101859312+GayatriWa@users.noreply.github.com>
2022-04-07 19:28:49 +0530

    Update kafkaproducer.py

--- kafkaproducer.py
+++ kafkaproducer.py
@@ -1,4 +1,5 @@
 from kafka import KafkaProducer
+import os
 bootstrap_servers = ['localhost:9092']
 topicName = 'myTopic'
 producer = KafkaProducer(bootstrap_servers = bootstrap_servers)
@@ -7,3 +8,9 @@ ack = producer.send(topicName, b'Hello World!!!!!!!!')
 metadata = ack.get()
 print(metadata.topic)
 print(metadata.partition)
+
+print(os.environ['JOB_NAME'])
+print(os.environ['BUILD_NUMBER'])
+print(os.environ['BUILD_URL'])
+print(os.environ['BUILD_TAG'])
+print(os.environ['BUILD_ID'])
