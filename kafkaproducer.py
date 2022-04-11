12f9cbff9a22c5506a96689df703a490b1b414b6
GayatriWa <101859312+GayatriWa@users.noreply.github.com>
2022-04-11 11:00:11 +0530

    Update kafkaproducer.py

--- kafkaproducer.py
+++ kafkaproducer.py
@@ -4,9 +4,9 @@ bootstrap_servers = ['localhost:9092']
 topicName = 'myTopic'
 producer = KafkaProducer(bootstrap_servers = bootstrap_servers)
 producer = KafkaProducer()
-ack = producer.send(topicName, b'Hello World!!!!!!!!',b'JOB_NAME')
+ack = producer.send(topicName, b'Hello World!!!!!!!!')
 metadata = ack.get()
 print(metadata.topic)
 print(metadata.partition)
-print(metadata.JOB_NAME)
+
 
